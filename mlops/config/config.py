3b9ff0c3deb11d3cf688290600632cf59aa11809
Nikhil <nikcena007@gmail.com>
2023-05-16 23:59:27 +0100

    Added Code in main.,py

--- mlops/config/config.py
+++ mlops/config/config.py
@@ -1,3 +1,11 @@
 from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent.absolute()
 CONFIG_DIR =Path(BASE_DIR,"Config")
+DATA_DIR=Path(BASE_DIR,"data")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# Assets
+PROJECTS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/projects.csv"
+TAGS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/tags.csv"
---
+++ mlops/tagifai/main.py
@@ -0,0 +1,25 @@
+# tagifai/main.py
+import pandas as pd
+from pathlib import Path
+import warnings
+
+import logging
+from config import config
+from tagifai import utils
+
+warnings.filterwarnings("ignore")
+
+def elt_data():
+    """Extract, load and transform our data assets."""
+    # Extract + Load
+    projects = pd.read_csv(config.PROJECTS_URL)
+    tags = pd.read_csv(config.TAGS_URL)
+    projects.to_csv(Path(config.DATA_DIR, "projects.csv"), index=False)
+    tags.to_csv(Path(config.DATA_DIR, "tags.csv"), index=False)
+
+    # Transform
+    df = pd.merge(projects, tags, on="id")
+    df = df[df.tag.notnull()]  # drop rows w/ no tag
+    df.to_csv(Path(config.DATA_DIR, "labeled_projects.csv"), index=False)
+
+    logger.info("✅ Saved data!")
