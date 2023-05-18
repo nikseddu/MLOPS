17d42331c162b2602df9e1b585cfef2f8e354ad7
Nikhil <nikcena007@gmail.com>
2023-05-18 20:00:31 +0100

    Split done

--- mlops/tagifai/data.py
+++ mlops/tagifai/data.py
@@ -6,6 +6,11 @@ from config import config
 
 from collections import Counter
 
+# tagifai/data.py
+import json
+import numpy as np
+
+
 
 
 def preprocess(df, lower, stem, min_freq):
@@ -67,3 +72,59 @@ def replace_minority_labels(df, label_col, min_freq, new_label="other"):
     df[label_col] = df[label_col].apply(lambda label: label if label in labels_above_freq else None)
     df[label_col] = df[label_col].fillna(new_label)
     return df
+
+
+
+class LabelEncoder(object):
+    """Encode labels into unique indices."""
+    def __init__(self, class_to_index={}):
+        self.class_to_index = class_to_index or {}  # mutable defaults ;)
+        self.index_to_class = {v: k for k, v in self.class_to_index.items()}
+        self.classes = list(self.class_to_index.keys())
+
+    def __len__(self):
+        return len(self.class_to_index)
+
+    def __str__(self):
+        return f"<LabelEncoder(num_classes={len(self)})>"
+
+    def fit(self, y):
+        classes = np.unique(y)
+        for i, class_ in enumerate(classes):
+            self.class_to_index[class_] = i
+        self.index_to_class = {v: k for k, v in self.class_to_index.items()}
+        self.classes = list(self.class_to_index.keys())
+        return self
+
+    def encode(self, y):
+        encoded = np.zeros((len(y)), dtype=int)
+        for i, item in enumerate(y):
+            encoded[i] = self.class_to_index[item]
+        return encoded
+
+    def decode(self, y):
+        classes = []
+        for i, item in enumerate(y):
+            classes.append(self.index_to_class[item])
+        return classes
+
+    def save(self, fp):
+        with open(fp, "w") as fp:
+            contents = {"class_to_index": self.class_to_index}
+            json.dump(contents, fp, indent=4, sort_keys=False)
+
+    @classmethod
+    def load(cls, fp):
+        with open(fp, "r") as fp:
+            kwargs = json.load(fp=fp)
+        return cls(**kwargs)
+
+from sklearn.model_selection import train_test_split
+
+def get_data_splits(X, y, train_size=0.7):
+    """Generate balanced data splits."""
+    X_train, X_, y_train, y_ = train_test_split(
+        X, y, train_size=train_size, stratify=y)
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_, y_, train_size=0.5, stratify=y_)
+    return X_train, X_val, X_test, y_train, y_val, y_test
